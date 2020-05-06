ddf09b9583ffe4209316999bde63a8cb1eeb2ba3
jijo66 <grggitau8@gmail.com>
2020-05-06 09:52:23 +0300

    Update description

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. 
+    <p> Deploying a simple React APP to AWS Amplify. A CI/CD tool</p>
         </p>
         <a
           className="App-link"
